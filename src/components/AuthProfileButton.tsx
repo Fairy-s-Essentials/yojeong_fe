e5b407c66e00732039c367e3b4d57253525ea908
Legitgoons <cksxkr5193@naver.com>
2025-11-05 01:20:30 +0900

    feat: 로그인/로그아웃 관련 컨테이너 컴포넌트 추가
    - AuthProfileButton: 로그인된 사용자용 (로그아웃 기능 포함)
    - LoginButton: 로그인 버튼 (카카오 로그인 Dialog 포함)
    - ProfileIcon을 활용한 컴포지션 패턴 적용

---
+++ src/components/AuthProfileButton.tsx
@@ -0,0 +1,58 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from './Dialog';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from './AlertDialog';
+import ProfileIcon from './ProfileIcon';
+import { useProfileAuth } from '@/hooks/auth/useProfileAuth';
+
+const AuthProfileButton = () => {
+  const {
+    user,
+    isLogoutAlertOpen,
+    setIsLogoutAlertOpen,
+    handleProfileClick,
+    handleLogout,
+  } = useProfileAuth();
+
+  return (
+    <>
+      <ProfileIcon
+        user={user}
+        onClick={handleProfileClick}
+      />
+      {/* 로그아웃 확인 알럿 */}
+      <AlertDialog open={isLogoutAlertOpen} onOpenChange={setIsLogoutAlertOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>로그아웃</AlertDialogTitle>
+            <AlertDialogDescription>
+              정말 로그아웃 하시겠습니까?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>취소</AlertDialogCancel>
+            <AlertDialogAction onClick={handleLogout}>
+              로그아웃
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+};
+
+export default AuthProfileButton;
+
