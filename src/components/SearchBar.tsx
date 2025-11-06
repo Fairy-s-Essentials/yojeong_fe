92b9f9c05079d0e8cae078f962e5e640e6361839
areumH <dkfmal0109@gmail.com>
2025-11-06 14:37:35 +0900

    design: 서치 바 컴포넌트 구현 및 페이지에 연결

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,18 @@
-const SearchBar = () => {
-  return <div>i</div>;
+import { Search } from 'lucide-react';
+import { cn } from '@/utils/cn';
+import Input from './Input';
+
+export interface SearchBarProps extends React.ComponentProps<'input'> {
+  className?: string;
+}
+
+const SearchBar = ({ className, ...props }: SearchBarProps) => {
+  return (
+    <div className={cn('relative w-full', className)}>
+      <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-app-gray-400 pointer-events-none" />
+      <Input {...props} className='pl-10' />
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { Header, SelectBox, StatisticCard } from '@/components';
+import { Header, SearchBar, SelectBox, StatisticCard } from '@/components';
 
 export const HistoryPage = () => {
   const [dateRange, setDateRange] = useState<string | number>(7);
+  const [inputValue, setInputValue] = useState<string>('');
 
   const dateOptions = [
     { value: 7, label: '최근 7일' },
@@ -11,18 +12,35 @@ export const HistoryPage = () => {
   ];
 
   return (
-    <div>
+    <div className="min-h-screen">
       <Header />
 
       <main className="flex flex-col items-center max-w-6xl mx-auto px-6 py-10">
+        {/* 드롭 다운 영역 */}
         <div className="w-full flex justify-end mb-6">
           <SelectBox value={dateRange} onChange={setDateRange} options={dateOptions} />
         </div>
+
         {/* 학습 통계 영역 */}
         <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-6 mb-16">
           <StatisticCard type="weekCount" size="lg" value="0개" />
           <StatisticCard type="accuracy" size="lg" value="0%" />
           <StatisticCard type="streak" size="lg" value="12일" />
+        </div>
+
+        {/* 학습 기록 영역 */}
+        <div className="w-full flex flex-col">
+          <div className="flex items-center justify-between mb-4">
+            <h2 className="text-app-gray-800">학습 기록</h2>
+
+            {/* 토글 컴포넌트 추가 */}
+          </div>
+
+          <SearchBar
+            placeholder="제목이나 배운점으로 검색..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
         </div>
       </main>
     </div>
