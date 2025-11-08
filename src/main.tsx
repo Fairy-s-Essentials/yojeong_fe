9adab259f50de3111c44d08b3e123c2349b00063
Legitgoons <cksxkr5193@naver.com>
2025-11-09 01:49:04 +0900

    chore: pre-commit 테스트

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Router } from "@router/Router";
-import { LoadingProvider } from "@/contexts";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Router } from '@router/Router';
+import { LoadingProvider } from '@/contexts';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <LoadingProvider>
@@ -16,5 +16,5 @@ createRoot(document.getElementById("root")!).render(
         <ReactQueryDevtools initialIsOpen={false} />
       </LoadingProvider>
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
